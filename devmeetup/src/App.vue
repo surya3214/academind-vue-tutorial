add7611a208240bd6f904d6cf52d9ca8cd91fc2f
Surya <surya3214@gmail.com>
2018-05-18 21:59:06 +0530

    vue-router setup

--- devmeetup/src/App.vue
+++ devmeetup/src/App.vue
@@ -2,27 +2,35 @@
   <v-app>
     <v-navigation-drawer v-model="sidenav" temporary fixed>
       <v-list>
-        <v-list-tile>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          router
+          :to="item.link">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>View Meetups</v-list-tile-content>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark class="purple darken-1">
       <v-toolbar-side-icon @click="sidenav = !sidenav" class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+        DevMeetup
+        </router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>
-          View Meetups
+        <v-btn flat v-for="item in menuItems" :key="item.title" router :to="item.link">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <main>
-
+      <router-view></router-view>
     </main>
   </v-app>
 </template>
@@ -31,7 +39,34 @@
 export default {
   data () {
     return {
-      sidenav: false
+      sidenav: false,
+      menuItems: [
+        {
+          icon: 'supervisor_account',
+          title: 'View Meetups',
+          link: '/meetups'
+        },
+        {
+          icon: 'room',
+          title: 'Organize Meetup',
+          link: '/meetups/register'
+        },
+        {
+          icon: 'person',
+          title: 'Profile',
+          link: '/profile'
+        },
+        {
+          icon: 'face',
+          title: 'Sign Up',
+          link: '/signup'
+        },
+        {
+          icon: 'lock_open',
+          title: 'Sign In',
+          link: '/signin'
+        }
+      ]
     }
   },
   name: 'App'
